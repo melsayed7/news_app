5e662a846e599c0cec62a1be312a6d5f99d1fce0
melsayed7 <mohamedelbiomy336@gmail.com>
2022-12-15 11:30:43 +0200

    add splash screen to project.

--- lib/component/style/my_Theme.dart
+++ lib/component/style/my_Theme.dart
@@ -1,20 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:news_app/component/style/my_color.dart';
 
 class MyTheme {
-  static Color primaryColor = Color(0xff39A552);
-  static Color whiteColor = Color(0xffffffff);
-  static Color blackColor = Color(0xff303030);
-  static Color textColor = Color(0xff4F5A69);
-  static Color sportColor = Color(0xffC91C22);
-  static Color healthColor = Color(0xffED1E79);
-  static Color enviromentColor = Color(0xff4882CF);
-  static Color politicsColor = Color(0xff003E90);
-  static Color businessColor = Color(0xffCF7E48);
-  static Color scienceColor = Color(0xffF2D352);
 
   static ThemeData lightTheme = ThemeData(
     appBarTheme: AppBarTheme(
-      color: primaryColor,
+      color: MyColor.primaryColor,
       centerTitle: true,
       shape: RoundedRectangleBorder(
         borderRadius: BorderRadius.only(
@@ -25,7 +16,7 @@ class MyTheme {
     ),
     textTheme: TextTheme(
       headline1: TextStyle(
-        color: whiteColor,
+        color: MyColor.whiteColor,
         fontSize: 22,
         fontWeight: FontWeight.w600,
       ),
